d9803cdb38349e54822b5dcfe4fe478c775284fb
MC2310300 <mlcalvert47@gmail.com>
2020-09-01 21:58:07 -0700

    Signed-off-by: MC2310300 <mlcalvert47@gmail.com>

---
+++ Lab/IntroLab/Hello_World/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Melissa
+ *
+ * Created on September 1, 2020, 9:26 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello World";
+    return 0;
+}
+
